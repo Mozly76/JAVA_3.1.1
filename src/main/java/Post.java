8fd51081fd7de3ccb89b451d67dab1b8c47f69e7
Sergey Kiselev <mozly76@yandex.ru>
2022-07-12 22:26:20 +0300

    Add classes Main, Post, FormDate

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public FormDate birthday; // поле для даты рождения
+    public String name; // поле для имени
+    public String passport; // поле для серии и номера паспорта
+    public String patronymic; // поле для отчества
+    public String phone; // поле для номера телефона
+    public String surname; // поле для фамилии
+    public boolean subscription; // поле о согласии на СМС информирование
+}
